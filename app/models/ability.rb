e0cade42448a2707f1be1fb9e9df4eba7387789c
atooshi_jun <ito@atooshi-works.com>
2022-08-04 14:46:10 +0900

    redmine_admin削除

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user && user.admin?
-      can :access, :rails_admin
-      can :manage, :all
-    end
+    # if user && user.admin?
+    #   can :access, :rails_admin
+    #   can :manage, :all
+    # end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
